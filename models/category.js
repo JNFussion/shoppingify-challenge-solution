cb20dcea5d2739723a7ef90f1504e96f6936e23e
JNFussion <juan.jose.gutierrez.99@gmail.com>
2022-04-13 12:16:57 +0200

    add unique to category model

--- models/category.js
+++ models/category.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var CategorySchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, unique: true, required: true, dropDups: true },
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
