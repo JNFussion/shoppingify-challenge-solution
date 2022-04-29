3ed550969c1ddc62c81a0a27b31ecaebbb1229a8
JNFussion <juan.jose.gutierrez.99@gmail.com>
2022-04-29 13:28:48 +0200

    refactor app file

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ require("./mongoConfig");
 
 var app = express();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 const categoryRouter = require("./routes/category");
 const itemRouter = require("./routes/item");
 
@@ -15,10 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
 app.use("", categoryRouter);
 app.use("", itemRouter);
